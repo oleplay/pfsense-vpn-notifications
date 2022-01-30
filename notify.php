e0e51075645e3199263e5be3fce908cda489e617
oleplay <ole.schicketanz@gmx.de>
2022-01-31 00:12:06 +0100

    Add Telegram Notification

--- notify.php
+++ notify.php
@@ -4,5 +4,6 @@ $args = getopt("",array("message::","subject::"));
 require_once("/etc/inc/notices.inc");
 //notify_all_remote("OPEN-VPN Connected");
 send_smtp_message($args["message"], $args["subject"]);
-
+notify_via_telegram($args["message"]);
+  
 ?>
